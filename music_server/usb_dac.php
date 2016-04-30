ad088d1128876864e134554e6f78fb58d23ea171
kozo yamagata <tune002@gmail.com>
2016-04-30 19:00:09 +0900

    Fix syntax

--- music_server/usb_dac.php
+++ music_server/usb_dac.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 左 USBDAC
 */
 
-exec('which gpio > /dev/null 2>&1 && gpio -g mode 14 out && gpio -g mode 15 out && gpio -g write 14 0 && gpio -g write 15 0')
+exec('which gpio > /dev/null 2>&1 && gpio -g mode 14 out && gpio -g mode 15 out && gpio -g write 14 0 && gpio -g write 15 0');
 
 http_response_code(201);
 echo json_encode(["status" => 201]);
