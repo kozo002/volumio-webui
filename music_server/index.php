47cc9ffed3ca5ded70d47004bc8ce316318a3cd9
kozo yamagata <tune002@gmail.com>
2016-04-30 19:03:30 +0900

    Add viewport

--- music_server/index.php
+++ music_server/index.php
@@ -3,6 +3,7 @@
   <head lang="ja">
     <meta charset="utf-8" />
     <title>RasPi Music Server</title>
+    <meta name='viewport' content='width=device-width initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="css/falt-ui.css" />
     <link rel="stylesheet" href="css/panels.css">
